a983f63522e1b35752c32725b8fdfb55e0ed5828
Pawan Nishad <anshpn8@gmail.com>
2024-09-17 21:46:54 +0530

    edit about

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,5 @@
 
 let apiCard = "https://script.google.com/macros/s/AKfycbz-4yQxkeQ-2ksMd-B1WTUpIt9MQ_by_M0E2WSV81KhToxGIVABIFyy_1_y8N5yx2h9/exec";
-
-
-
 const postContainer1= document.querySelector('#testid');
 const postMethod1 =()=>{
     // Use fetch to make a GET request to the API endpoint and parse the JSON response
@@ -11,20 +8,18 @@ const postMethod1 =()=>{
     .then(data => {
         // Extract the todo data from the response
         // let cardData1 = data.card;
-
         // Map each todo item to a string of table row HTML
         let data1=data.cardData;
-     
         for(let i=0;i<=data.cardData.length;i++){
-            console.log(data1[0]);
         const postElement1= document.createElement('div');
-        postElement1.classList.add('.cards1');
-        postElement1.innerHTML=`    
-                                <div class="col-lg-4 col-md-6 mb-4">
+        postElement1.classList.add('col-lg-4');
+        postElement1.classList.add('col-md-6');
+        postElement1.classList.add('mb-4');
+        postElement1.innerHTML=`
                                 <div class="serv-cove shadow-md rounded bg-white p-3">
                                      <div class="prf row mb-3">
                                          <div class="col-md-3">
-                                             <img class="rounded-pill" src="${data.cardData[i][0]}" alt="not available">
+                                             <img class="rounded-pill" src="https://drive.google.com/uc?export=view&id=${data.cardData[i][0]}&export=view&authuser=0" alt="not available">
                                          </div>
                                          <div class="col-md-9 align-self-center">
                                              <h6 class="mb-0 fw-bolder">${data.cardData[i][1]}</h6>
@@ -47,9 +42,8 @@ const postMethod1 =()=>{
                                      </div>
                                 </div>
                              </div>
-                             </div>`
+                             `
                              postContainer1.appendChild(postElement1);
-
                     }
         });
 }
